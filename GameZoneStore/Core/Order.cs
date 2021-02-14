f8a864274562ab9e0bb7a4236861573b47ee5850
drincon2 <diegorima98@hotmail.com>
2021-02-13 22:36:30 -0700

    Added ordertime and products list fields. Added methods AddProduct() to add products to order, CheckProductQuantity() to check for high quantities of the same product, and RejectOrder() to reject orders with high quantities of the same product.

--- GameZoneStore/Core/Order.cs
+++ GameZoneStore/Core/Order.cs
@@ -4,7 +4,69 @@ using System.Text;
 
 namespace Core
 {
-    class Order
+    internal class Order
     {
+        private DateTime _ordertime;
+        private List<Product> products = new List<Product>();
+
+        // Retrieve and set the time of order placement
+        public DateTime OrderTime { get; set;}
+
+        // AddProduct(): Add different types of products to the order
+        // CheckProductQuantity(): Helper method of AddProduct(). Currently checks product of same type and name does not have more than 10 occurences. 
+        //                         Otherwise, it rejects the order. Will later use inventory of Location as a constraint rather than the 10 occurences in list.
+        public void AddProduct(string kind, string name, int id, double cost)
+        {
+            CheckProductQuantity();
+            
+            Product item = new Product();
+            item.Type = kind;
+            item.Name = name;
+            item.Id = id;
+            item.Price = cost;
+            products.Add(item);
+
+        }
+
+        public void CheckProductQuantity()
+        {
+            if (products.Count != 0)
+            {
+                int duplicateCount = 0;
+
+                foreach (Product p1 in products)
+                {
+                    foreach (Product p2 in products)
+                    {
+                        if (p2.Name.Equals(p1.Name) && p2.Type.Equals(p1.Type))
+                        {
+                            duplicateCount++;
+
+                            if (duplicateCount == 10)
+                            {
+                                RejectOrder(p1.Name, p1.Type);
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        // Currently erases all items of a list
+        public void RejectOrder(string itemName, string itemTag)
+        {
+            Console.WriteLine("Order has exceeded limit for item " + itemName + " from " + itemTag + " section" +
+                               "\nOrder has been rejected and deleted. Please check the quanity limits and " + 
+                               "availability tags to avoid this situation in the future" + 
+                               "Thank you for visiting GameZone.");
+            products.Clear();
+        }
+
+
     }
 }
