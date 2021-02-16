53738b584856e5f328bed72d028c848b686f0e76
drincon2 <diegorima98@hotmail.com>
2021-02-16 16:31:18 -0700

    Added field Quantity to facilitate product quantity checking inthe  class Order, and update of inventory product quantity in the class Location.

--- GameZoneStore/Core/Product.cs
+++ GameZoneStore/Core/Product.cs
@@ -10,7 +10,7 @@ namespace Core
         public string Name { get; set; }
         public int Id { get; set; }
         public double Price { get; set; }
-
+        public int Quantity { get; set; }
 
 
     }
