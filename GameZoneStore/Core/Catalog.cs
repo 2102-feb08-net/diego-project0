5d7c7b8614add4fb3a75f018fc2d9f4a083fdec4
drincon2 <diegorima98@hotmail.com>
2021-02-18 23:37:10 -0700

    Added Catalog class to add items and customer name to an order, and check out order. Also added ProcessOrder class to validate and provide a summary of the order.

---
+++ GameZoneStore/Core/Catalog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class Catalog
+    {
+        // Order
+        private Order cart = new Order();
+        // Location
+        private Location defaultLocation = new Location();
+        // Catalog
+        private List<Product> catalog = new List<Product>();
+        // Data Access Catalog Repo
+
+        // Change Location
+        
+        // Display Catalog
+
+        // Browse by category
+        
+        // Add to cart
+        public void AddToCart(Product item)
+        {
+            cart.AddProduct(item.Type, item.Name, item.Id, item.Price, item.Quantity);
+        }
+
+        // Check out
+        public void CheckOut(ProcessOrder cashier, String customerName)
+        {
+            if(cashier.IsOrderValid(cart, defaultLocation))
+            {
+                cashier.OrderSummary(cart);
+                cart.SetCustomerName(customerName);
+                cashier.PlaceOrder(cart); 
+            }
+        }
+
+    }
+}
---
+++ GameZoneStore/Core/ProcessOrder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class ProcessOrder
+    {
+        // Review Order
+        public bool IsOrderValid(Order cart, Location localRetail)
+        {
+            // Check product quantity in order
+            cart.CheckProductQuantity();
+
+            // Check inventory product availability for products in the order
+            if (localRetail.IsInventoryUpdated(cart.Products))
+            {
+                return true;
+            }
+            
+            return false;
+            
+        }
+
+        // Order Summary
+        public void OrderSummary(Order cart)
+        {
+            double orderTotal = 0.0;           
+            
+            foreach (Product item in cart.Products)
+            {
+                orderTotal += item.Price; 
+                Console.WriteLine("Item: " + item.Name + "\tQuantity: " + item.Quantity + "\tPrice: " + item.Price);
+            }
+
+            Console.WriteLine("Total: $" + String.Format("{0:0.00}", orderTotal));
+
+        }
+
+        // Place Order
+        public void PlaceOrder(Order cart)
+        {
+            // Save customer information
+
+            // Save/update retail store information
+
+            // Order Placement Confirmation
+            Console.WriteLine("Your order has been placed! Your package sould arrive between 3 and 5 days.");
+        }
+        
+    }
+}
