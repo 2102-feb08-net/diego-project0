4ac3dc380a5a01ab3f583dc1d78a259dfdf86ba1
drincon2 <diegorima98@hotmail.com>
2021-02-19 00:46:19 -0700

    Renamed classes CustomerHistory and StoreHistory to StoreCustomerHistory and StoreLocationHistory. Added basic structure of the latter to communicate with the Data Access Layer.

---
+++ GameZoneStore/Core/StoreCustomerHistory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class StoreCustomerHistory : StoreHistory
+    {
+        // Show Customer History
+        public void ShowOrderHistory()
+        {
+
+        }
+
+        // Search for a specific customer order history by name
+        public void SearchByName()
+        {
+
+        }
+
+        // Add customer to DB
+        public void AddCustomer(Customer clientRecord)
+        {
+
+        }
+    }
+}
