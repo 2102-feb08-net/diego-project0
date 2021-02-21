a6b32901b5315b9c06fc72c8b87a735a2a3eecd9
drincon2 <diegorima98@hotmail.com>
2021-02-21 01:52:28 -0700

    Set up Entity Framework for project and added current tables from database.

---
+++ GameZoneStore/EFDBGameZoneStore.DataAccess/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFDBGameZoneStore.DataAccess.Entities
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ GameZoneStore/EFDBGameZoneStore.DataAccess/Entities/project0rincongamezonestoreContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EFDBGameZoneStore.DataAccess.Entities
+{
+    public partial class project0rincongamezonestoreContext : DbContext
+    {
+        public project0rincongamezonestoreContext()
+        {
+        }
+
+        public project0rincongamezonestoreContext(DbContextOptions<project0rincongamezonestoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product", "GZStore");
+
+                entity.Property(e => e.ProductId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ProductID");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Price).HasColumnType("numeric(10, 2)");
+
+                entity.Property(e => e.Type).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
