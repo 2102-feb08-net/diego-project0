0cd148bcab6d5eada49e009c2f500d82bb6358eb
drincon2 <diegorima98@hotmail.com>
2021-02-22 00:57:13 -0700

    Added interface ICustomerRepository to add customer to database.

---
+++ GameZoneStore/Core/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        // Add Customer to Database
+        public void AddCustomer(Customer cust);
+
+        // Save changes to Database
+        public void Save();
+    }
+}
