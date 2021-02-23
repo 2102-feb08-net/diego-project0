7692ec416af1b97f18dc63c3b2e608ec501010da
drincon2 <diegorima98@hotmail.com>
2021-02-22 23:51:07 -0700

    Added constructor and validation for fields.

--- GameZoneStore/Core/Models/Customer.cs
+++ GameZoneStore/Core/Models/Customer.cs
@@ -6,6 +6,42 @@ namespace Core
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int CustomerId { get; set; }
+
+        public Customer(string firstName, string lastName)
+        {
+            if (ValidateName(firstName, lastName))
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+        }
+
+        public bool ValidateName(string fname, string lname)
+        {
+            // Validate customer name
+            if (fname is null || lname is null)
+            {
+                throw new ArgumentNullException("First and last name must not be empty.", nameof(fname));
+            }
+            
+            foreach(char letter in fname)
+            {
+                if (!(Char.IsLetter(letter)))
+                {
+                    throw new ArgumentException("Use letters only.", nameof(fname));
+                }
+            }
+
+            foreach(char letter in lname)
+            {
+                if (!(Char.IsLetter(letter)))
+                {
+                    throw new ArgumentException("Use letters only.", nameof(lname));
+                }
+            }
+            
+            return true;
+        }
     }
 }
